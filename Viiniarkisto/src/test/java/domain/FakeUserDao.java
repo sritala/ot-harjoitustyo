604c13ae18c442ab1954fcdd95e23f279cb520b8
sritala <susanna.ritala@hotmail.com>
2019-05-05 17:19:29 +0300

    Update FakeUserDao.java

--- Viiniarkisto/src/test/java/domain/FakeUserDao.java
+++ Viiniarkisto/src/test/java/domain/FakeUserDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 import java.util.ArrayList;
@@ -11,23 +6,24 @@ import wine.dao.UserDao;
 import wine.domain.User;
 
 public class FakeUserDao implements UserDao {
+
     List<User> users = new ArrayList<>();
 
     public FakeUserDao() {
         users.add(new User("testertester"));
     }
-    
+
     @Override
     public User findByUsername(String username) {
-        return users.stream().filter(u->u.getUsername().equals(username)).findFirst().orElse(null);
+        return users.stream().filter(u -> u.getUsername().equals(username)).findFirst().orElse(null);
     }
-    
+
     @Override
     public User create(User user) {
         users.add(user);
         return user;
-    } 
-    
+    }
+
     @Override
     public List<User> getAll() {
         return users;
